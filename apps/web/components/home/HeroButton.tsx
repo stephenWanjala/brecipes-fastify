732d25b0d88cdd8fbbc92f23419e4d1221c77e1f
Wanjala Stephen <stephenwanjala145@gmail.com>
2025-06-09 16:04:27 +0300

    Add HeroButton component for conditional rendering based on user authentication

---
+++ apps/web/components/home/HeroButton.tsx
@@ -0,0 +1,27 @@
+"use client"
+import React from "react";
+import PropTypes from "prop-types";
+import { Button } from "../ui/button";
+import Link from "next/link";
+import { useAuth } from "@/context/auth-context";
+
+function HeroButton({ text, route }) {
+    const { user } = useAuth();
+
+    if (user) return null;
+
+    return (
+        <Link href={route}>
+            <Button variant="outline" size="lg" className="px-8 font-medium">
+                {text}
+            </Button>
+        </Link>
+    );
+}
+
+HeroButton.propTypes = {
+    text: PropTypes.string.isRequired,
+    route: PropTypes.string.isRequired,
+};
+
+export default HeroButton;
